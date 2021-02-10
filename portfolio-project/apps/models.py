ff7bfe8827ce314f5cb77c269e10d2a8d00b6795
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-10 14:28:07 +0100

    add apps app with models + URLFIELD fix

--- portfolio-project/apps/models.py
+++ portfolio-project/apps/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 
-class Blog(models.Model):
+class Apps(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     url = models.URLField(max_length=200)
-
--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
     'blog.apps.BlogConfig',
+    'blog.apps.AppsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
