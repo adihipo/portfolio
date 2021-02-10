5c43171c21fd8d9507b4c6ec8e714555f82741e9
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-10 16:56:50 +0100

    init apps, pass info

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Job
+from apps.models import App
 
 
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs': jobs})
+    apps = App.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs, 'apps': apps})
