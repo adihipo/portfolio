eb427813035ea5a3c0692f8cb0c2e4a4d3fb5408
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-10 14:01:08 +0100

    change to postgres db

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('POSTGRES-PW'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
