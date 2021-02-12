6ef9f94d787a1cebf56cd0d4807e596bc8780f57
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-12 20:17:00 +0100

    minor change in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
