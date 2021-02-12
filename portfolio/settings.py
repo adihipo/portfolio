3337fb4f01eca0a49f0033354fbd90efc66e2076
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-12 20:50:33 +0100

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
